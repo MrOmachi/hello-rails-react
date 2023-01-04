7168977c2813b3662555a8365bec898c0ab31fd8
Omachi John <omachi.john@yahoo.com>
2023-01-04 14:42:15 -0800

    setup greetings with app

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
+  root 'message#index'
+
+  get 'greeting/random', to: 'message#greeting' 
   # Defines the root path route ("/")
   # root "articles#index"
 end
